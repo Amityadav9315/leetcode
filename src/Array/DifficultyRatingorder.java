1ea4f8f18399011e1e8cf6f56ec62e37cac4a2e7
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-06-22 11:31:59 +0530

    DifficultyRting

---
+++ src/Array/DifficultyRatingorder.java
@@ -0,0 +1,36 @@
+package Array;
+
+import java.util.Scanner;
+
+public class DifficultyRatingorder {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int count = 0;
+        int[] d = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            d[i] = scanner.nextInt();
+
+        }
+        for (int i = 0; i <d.length-1; i++) {
+            if (d[i] <= d[i + 1]) {
+                count++;
+            }
+        }
+            if (count == n - 1) {
+                System.out.println("Yes");
+            } else {
+                System.out.println("No");
+            }
+
+
+        }
+
+        // Your code goes here
+    }
+
+
+
+
+
