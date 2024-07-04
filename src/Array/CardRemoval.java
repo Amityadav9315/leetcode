155be4094c82f713c756556e91452abd4ef6075e
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-07-04 09:29:59 +0530

    CardRemoval

---
+++ src/Array/CardRemoval.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class CardRemoval{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+            int n=sc.nextInt();
+            int count=n;
+            int[] arr=new int[n];
+            for(int i=0; i<n; i++){
+                arr[i]=sc.nextInt();
+            }
+            for(int i=0; i<n; i++) {
+                for (int j = i + 1; j <= n - 1; j++) {
+                    if (arr[i] != arr[j]) {
+                        count++;
+                        break;
+                    }
+                }
+            }
+            System.out.println(count);
+        }
+    }
+
