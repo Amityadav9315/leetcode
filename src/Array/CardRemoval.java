7f3034decbb5cb571d5124bb6413645172f951ca
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-07-05 19:12:49 +0530

    Countingprettynumber

--- src/Array/CardRemoval.java
+++ src/Array/CardRemoval.java
@@ -3,7 +3,7 @@ public class CardRemoval{
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
             int n=sc.nextInt();
-            int count=n;
+            int count=0;
             int[] arr=new int[n];
             for(int i=0; i<n; i++){
                 arr[i]=sc.nextInt();
@@ -12,6 +12,9 @@ public class CardRemoval{
                 for (int j = i + 1; j <= n - 1; j++) {
                     if (arr[i] != arr[j]) {
                         count++;
+
+                    }
+                    if(count<n){
                         break;
                     }
                 }
---
+++ src/Array/Countingprettynumber.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class Countingprettynumber {
+        public static void main(String[] args) throws java.lang.Exception {
+            Scanner sc = new Scanner(System.in);
+
+            int t = sc.nextInt();
+
+            while (t-- > 0) {
+                int n = sc.nextInt();
+                int m=sc.nextInt();
+                int count = 0;
+
+                for (int i = n; i <= m; i++) {
+                    int c=i%10;
+                    if(c==2||c==3||c==9){
+                        count++;
+                    }
+                }
+
+                System.out.println(count);
+            }
+
+
+        }
+    }
+
+
