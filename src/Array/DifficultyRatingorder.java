8cacca27c7c73181fb91dc7ca39a4ab155e491eb
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-07-11 21:38:43 +0530

    DifficulytRatingorder

--- src/Array/DifficultyRatingorder.java
+++ src/Array/DifficultyRatingorder.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class DifficultyRatingorder {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the value of n");
         int n = scanner.nextInt();
         int count = 0;
         int[] d = new int[n];
