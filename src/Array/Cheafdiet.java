bda28309c3b2d5958f0d1209d119030572fc3c52
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-07-18 18:25:08 +0530

    Cheafdiet

---
+++ src/Array/Cheafdiet.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import java.lang.*;
+    public static  void main (String[] args) throws java.lang.Exception
+    {
+        Scanner sc=new Scanner(System.in);
+            int n=sc.nextInt();
+            int k=sc.nextInt();
+            int count=0;
+           // int d=0;
+            int[] arr=new int[n];
+            for(int i=1; i<=n; i++){
+                arr[i]=sc.nextInt();
+
+            }
+            for(int i=1; i<=arr.length; i++){
+                if(arr[i]>=k){
+                    count++;
+                    int c=arr[i]-k;
+                    arr[i+1]=c+arr[i];
+
+                }
+                else if(arr[i]<k){
+                     count=0;
+                    int  d=arr[i];
+
+                }
+
+            }
+            if(count==n){
+                System.out.println("Yes");
+            }
+            if(count==0){
+                System.out.println("No"+d);
+            }
+            // your code goes here
+
+        }
+
+
