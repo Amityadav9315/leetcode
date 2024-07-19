cc3ed6a527bcb7e0443a0a63f753d69d9c6eff87
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-07-19 23:53:15 +0530

    LuckeyClover

---
+++ src/Array/LuckeyClover.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int d=4+(n-1)*3;
+        System.out.println(d);
+        // your code goes here
+
+    }
+}
