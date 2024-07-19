7132dced87f24059a5054753112ba10a70151c43
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-07-19 23:54:40 +0530

    LuckeyClover

--- src/Array/LuckeyClover.java
+++ src/Array/LuckeyClover.java
@@ -7,6 +7,7 @@ class Codechef
     public static void main (String[] args) throws java.lang.Exception
     {
         Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the value of n");
         int n=sc.nextInt();
         int d=4+(n-1)*3;
         System.out.println(d);
