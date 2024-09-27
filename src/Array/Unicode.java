fb7931dcec5b0cb161b7976bc25952141497c37c
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-09-27 17:15:37 +0530

    Unicode

--- src/Array/Unicode.java
+++ src/Array/Unicode.java
@@ -4,6 +4,7 @@ public class Unicode {
     public static void main(String[] args) {
         for(char c=0x0320;c<=0x03FF;c++){
             System.out.println(c+" ");
+            System.out.println("yes");
         }
     }
 }
