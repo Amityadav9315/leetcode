9190047e86dc4fb0f59131cbcad15c0a217e4ac2
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-09-27 17:15:10 +0530

    Unicode

---
+++ src/Array/Unicode.java
@@ -0,0 +1,9 @@
+package Array;
+
+public class Unicode {
+    public static void main(String[] args) {
+        for(char c=0x0320;c<=0x03FF;c++){
+            System.out.println(c+" ");
+        }
+    }
+}
