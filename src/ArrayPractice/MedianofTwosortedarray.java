b7a2c142f1821c5ed8a3e02a899d962ac6515d61
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-12-09 01:32:17 +0530

    rbklgrkl

---
+++ src/ArrayPractice/MedianofTwosortedarray.java
@@ -0,0 +1,40 @@
+package ArrayPractice;
+
+public class MedianofTwosortedarray {
+    class Solution {
+        public double findMedianSortedArrays(int m,int n,int[] nums1, int[] nums2) {
+
+            int[] merge=new int[m+n];
+
+            for(int i=0; i<m; i++){
+                merge[i]=nums1[i];
+            }
+            for(int i=0; i<n; i++){
+                merge[i+nums1.length]=nums2[i];
+            }
+            int sum=0;
+            for(int i=0; i<merge.length; i++){
+                sum=sum+merge[i];
+            }
+            float medium=sum/merge.length;
+            System.out.println(medium);
+        }
+
+        public static void  main(String[] args){
+            Scanner sc=new Scanner(System.in);
+            int m=sc.nextInt();
+            int n=sc.nextInt();
+            int[] nums1=new int[m];
+            for(int i=0; i<m; i++){
+                nums1[i]=sc.nextint();
+            }
+            int[] nums2=new int[n];
+            for(int i=0; i<n; i++){
+                nums2[i]=sc.nextInt();
+            }
+            findMedianSortedArrays(m,n,nums1,nums2);
+        }
+
+    }
+
+}
