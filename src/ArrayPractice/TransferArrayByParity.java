8b7f1b7ca5f7b6efeddd8b18c6e3cce081a166ae
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2025-03-02 11:59:35 +0530

    fqeb,geq

---
+++ src/ArrayPractice/TransferArrayByParity.java
@@ -0,0 +1,19 @@
+package ArrayPractice;
+
+import java.util.Scanner;
+
+public class TransferArrayByParity {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] nums=new int[n];
+        for(int i=0; i<n; i++){
+            nums[i]=sc.nextInt();
+        }
+        int[] result=sc.nextInt();
+
+    }
+
+
+}
