f0b5563c898aac22d2de67eadfe1d430ec217ff6
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2025-03-04 22:44:12 +0530

    db,dsblsd

--- src/ArrayPractice/FirstMissingPositive.java
+++ src/ArrayPractice/FirstMissingPositive.java
@@ -1,6 +1,21 @@
 package ArrayPractice;
 
+import java.util.Scanner;
+
 public class FirstMissingPositive{
+
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] nums=new int[n];
+        for(int i=0; i<n; i++){
+            nums[i]=sc.nextInt();
+        }
+
+    }
+    
+
 
 
 }
