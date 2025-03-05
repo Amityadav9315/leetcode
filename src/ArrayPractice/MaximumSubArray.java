f289f68a31f86cd9b27f94e53a8f6e95a43e07bb
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2025-03-06 01:11:34 +0530

    dvkjvsd

--- src/ArrayPractice/MaximumSubArray.java
+++ src/ArrayPractice/MaximumSubArray.java
@@ -26,7 +26,8 @@ public class MaximumSubArray {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
         int[] nums=new int[n];
-        for(int i=0; )
+        for(int i=0; i<n; i++){
+            nums[i]=sc.nextInt();
 
     }
     }
