f6355692bb44d932a7c0a5309294b183b9aba729
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2025-03-09 17:51:44 +0530

    dgrfgr

--- src/ArrayPractice/PascalTriangle.java
+++ src/ArrayPractice/PascalTriangle.java
@@ -6,6 +6,16 @@ import java.util.Scanner;
 
 public class PascalTriangle {
 
+
+    static  void pascalTriangle(int n){
+
+        for(int i=0;i<=n; i++){
+            for(int j=0; j<=n-i; j++){
+                System.out.println(" ");
+            }
+        }
+    }
+
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
