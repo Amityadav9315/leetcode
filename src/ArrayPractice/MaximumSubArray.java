9a774d7177f0114ed322c5f674e6bafb1a7fc4fd
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2025-03-09 17:36:32 +0530

    fgdhsjurtye

--- src/ArrayPractice/MaximumSubArray.java
+++ src/ArrayPractice/MaximumSubArray.java
@@ -28,7 +28,7 @@ public class MaximumSubArray {
 
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
+        int n=sc.nextInt();                                                                                                                                                                                                               
         int[] nums=new int[n];
         for(int i=0; i<n; i++){
             nums[i]=sc.nextInt();
---
+++ src/ArrayPractice/PascalTriangle.java
@@ -0,0 +1,8 @@
+package ArrayPractice;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class PascalTriangle {
+
+
+}
