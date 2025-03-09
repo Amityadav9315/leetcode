c750d7fcb297abc7d7db1f23a169cdb1964b90f8
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2025-03-09 17:49:07 +0530

    hfgdret

--- src/ArrayPractice/PascalTriangle.java
+++ src/ArrayPractice/PascalTriangle.java
@@ -2,7 +2,15 @@ package ArrayPractice;
 
 import org.w3c.dom.ls.LSOutput;
 
+import java.util.Scanner;
+
 public class PascalTriangle {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+
+    }
 
 
 }
